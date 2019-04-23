a235d2fc9d1582505b5b9deff6162800834f8107
cewdesign <woodce@gmail.com>
2019-04-23 10:47:10 -0400

    comment out tasks for testing

--- pipeline/scripts/01-remove.sh
+++ pipeline/scripts/01-remove.sh
@@ -1,21 +1,23 @@
-printf "Remove all non-films, non-subtitles."
+ls
 
-printf "Remove all sample files... "
-find . -type f -iname 'sample.avi' -delete
-echo "Complete."
+# printf "Remove all non-films, non-subtitles."
 
-printf "Remove .nfo files... "
-find . -type f -iname '*.nfo' -delete
-echo "Complete."
+# printf "Remove all sample files... "
+# find . -type f -iname 'sample.avi' -delete
+# echo "Complete."
 
-printf "Remove .txt files... "
-find . -type f -iname '*.txt' -delete
-echo "Complete."
+# printf "Remove .nfo files... "
+# find . -type f -iname '*.nfo' -delete
+# echo "Complete."
 
-printf "Remove proof image files... "
-find . -type f -iname '*proof.jpg' -delete
-echo "Complete."
+# printf "Remove .txt files... "
+# find . -type f -iname '*.txt' -delete
+# echo "Complete."
 
-printf "Remove .srt files... "
-find . -type f -iname '*.srt' -delete
-echo "Complete."
+# printf "Remove proof image files... "
+# find . -type f -iname '*proof.jpg' -delete
+# echo "Complete."
+
+# printf "Remove .srt files... "
+# find . -type f -iname '*.srt' -delete
+# echo "Complete."
